3eda0da20cea70032e57323fa897f898a652d5b1
Wesley Soares <wesleybs90@gmail.com>
2024-11-02 15:59:01 -0300

    Setup initial Angular project

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideHttpClient(withFetch()), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+};

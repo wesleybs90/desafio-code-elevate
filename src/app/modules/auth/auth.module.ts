3b908fe5a5781a5b595a2460e79447c46ae77a20
Wesley Soares <wesleybs90@gmail.com>
2024-11-04 00:52:41 -0300

    Add auth module

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthService } from './services/auth/auth.service';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule { }
